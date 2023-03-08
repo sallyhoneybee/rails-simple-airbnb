31c0c5c7e1c19e740294c13094e598f1961de017
Summer Chew <summerchewjx@gmail.com>
2023-03-08 15:22:46 +0800

    Generated contoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+  get 'flats/show'
+  get 'flats/new'
+  get 'flats/create'
+  get 'flats/edit'
+  get 'flats/update'
+  get 'flats/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
