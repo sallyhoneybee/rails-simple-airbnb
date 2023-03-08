09d7576b81322a4985c586e50c673b7e634636af
Summer Chew <summerchewjx@gmail.com>
2023-03-08 15:34:19 +0800

    created resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'flats/index'
-  get 'flats/show'
-  get 'flats/new'
-  get 'flats/create'
-  get 'flats/edit'
-  get 'flats/update'
-  get 'flats/delete'
+  resources :flats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
